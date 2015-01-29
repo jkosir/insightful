18ef391c1916c9347305ee98e63ad5a81baa094c
Jakob Košir <kosir.jakob@gmail.com>
2015-01-29 10:21:01 +0100

    cache settings

--- tracker/settings.py
+++ tracker/settings.py
@@ -31,11 +31,7 @@ TEMPLATE_DEBUG = DEBUG
 ALLOWED_HOSTS = ['*']
 
 # Use memcache via MemCachier on heroku, fallback to LocMem cache if MemCachier isn't set up (no environment variables)
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-    }
-}
+CACHES = memcacheify()
 
 # Application definition
 
